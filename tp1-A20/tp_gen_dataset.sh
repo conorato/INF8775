ba22b6a80689dc5b4e46f4b397dfab50c120e3f4
Claudia Onorato <claudia.onorato@polymtl.ca>
2020-10-09 01:22:21 -0400

    added sh to gen dataset

---
+++ tp1-A20/tp_gen_dataset.sh
@@ -0,0 +1,8 @@
+for i in {1..10}; do
+  let nsamples=100*2**i
+  echo "Generating a set with $nsamples couples"
+
+  for j in {1..10}; do
+    python gen.py $nsamples ./data/ex${nsamples}_$j.txt
+  done
+done
