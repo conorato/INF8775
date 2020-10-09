a26b9a7165e224a7646f508cc564f0e9431652d9
Claudia Onorato <claudia.onorato@polymtl.ca>
2020-10-08 23:44:30 -0400

    added algo codes

---
+++ tp1-A20/src/brute/closests_points_brute.cpp
@@ -0,0 +1,45 @@
+// A brute force program in C++ to find the smallest distance from a
+// given set of points.
+// Brute force algo source: https://www.geeksforgeeks.org/closest-pair-of-points-onlogn-implementation/
+
+#include <iostream>
+#include <float.h>
+#include <stdlib.h>
+#include <math.h>
+using namespace std;
+
+
+// A structure to represent a Point in 2D plane
+struct Point
+{
+	int x, y;
+};
+
+// A utility function to find the distance between two points
+float dist(Point p1, Point p2)
+{
+	return sqrt( (p1.x - p2.x)*(p1.x - p2.x) +
+				(p1.y - p2.y)*(p1.y - p2.y)
+			);
+}
+
+// The main function that finds the smallest distance
+// A Brute Force method to return the smallest distance between two points
+// in P[] of size n
+float closest(Point P[], int n)
+{
+	float min = FLT_MAX;
+	for (int i = 0; i < n; ++i)
+		for (int j = i+1; j < n; ++j)
+			if (dist(P[i], P[j]) < min)
+				min = dist(P[i], P[j]);
+	return min;
+}
+
+int main()
+{
+	Point P[] = {{2, 3}, {12, 30}, {40, 50}, {5, 1}, {12, 10}, {3, 4}};
+	int n = sizeof(P) / sizeof(P[0]);
+	cout << "The smallest distance is " << closest(P, n) << endl;
+	return 0;
+}
