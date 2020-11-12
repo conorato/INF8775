78e83fba3b0831a7889ff0bc5caed426d0dc2c21
Claudia Onorato <claudia.onorato@polymtl.ca>
2020-11-12 18:14:42 -0500

    seemsss to work, tower sequence has to be fixed

--- tp2-A20/prototype/dyn_prog.py
+++ tp2-A20/prototype/dyn_prog.py
@@ -18,43 +18,33 @@ def _execute_dyn_prog(blocs):
     """finds sequence of blocs in order to get the biggest tower height"""
     path = []
     blocs.sort(key=_get_surface, reverse=True)
+    blocs_indexes = list(range(len(blocs)))
 
     # computation array (each tower starts with its own height)
     tower_height = [bloc[0] for bloc in blocs]
     # side array to keep sequence (each tower starts with its own index)
     tower_sequence = [[bloc] for bloc in blocs]
 
-    def _find_bigger_bloc(bloc, index):
-        bigger_or_equal_surface_blocs = blocs[:index]
-        bigger_surface_bloc_indexes = [
-            idx
-            for idx, receiving_bloc in enumerate(bigger_or_equal_surface_blocs)
-            if is_stricly_smaller(bloc, receiving_bloc)
-        ]
-
-        if len(bigger_surface_bloc_indexes) == 0:
-            return None
-
-        highest_tower_idx = max(
-            bigger_surface_bloc_indexes, key=lambda i: blocs[i])
-
-        return highest_tower_idx
-
     current_threshold_index = 0
-    # while len(blocs[current_threshold_index:]) > 0:
-    for index, bloc in enumerate(blocs[current_threshold_index:]):
+    i = 1
+    while current_threshold_index != len(blocs):
+        print(f'Tower of height {i} with current table {tower_height}')
+        for index, bloc in enumerate(blocs[current_threshold_index:]):
+            receiving_bloc_index = _find_bigger_bloc(
+                blocs[current_threshold_index:], bloc, index
+            )
+            print(f'bloc {bloc} has receiving: {receiving_bloc_index}')
+            if receiving_bloc_index is not None:
+                tower_height[current_threshold_index +
+                             index] = tower_height[current_threshold_index + receiving_bloc_index] + bloc[0]
+                tower_sequence[current_threshold_index +
+                               index].append(blocs[current_threshold_index + receiving_bloc_index])
+            else:
+                next_threshold_index = current_threshold_index + index
 
-        # index += current_threshold_index
-        receiving_bloc_index = _find_bigger_bloc(bloc, index)
-        print(f'index {index} has receiving: {receiving_bloc_index}')
-        if receiving_bloc_index is not None:
-            tower_height[index] = tower_height[receiving_bloc_index] + bloc[0]
-            tower_sequence[index].append(blocs[receiving_bloc_index])
-        else:
-            threshold_index = index
-
-        # current_threshold_index = threshold_index + 2
-    # tmp += 1
+        current_threshold_index = next_threshold_index + 1
+        i += 1
+        print('current_threshold_index : ', current_threshold_index)
 
     tallest_tower_base_index = _arg_max(tower_height)
 
@@ -63,6 +53,23 @@ def _execute_dyn_prog(blocs):
 
 def _get_surface(bloc):
     return bloc[1] * bloc[2]
+
+
+def _find_bigger_bloc(blocs, bloc, index):
+    bigger_or_equal_surface_blocs = blocs[:index]
+    bigger_surface_bloc_indexes = [
+        idx
+        for idx, receiving_bloc in enumerate(bigger_or_equal_surface_blocs)
+        if is_stricly_smaller(bloc, receiving_bloc)
+    ]
+
+    if len(bigger_surface_bloc_indexes) == 0:
+        return None
+
+    highest_tower_idx = max(
+        bigger_surface_bloc_indexes, key=lambda i: blocs[i])
+
+    return highest_tower_idx
 
 
 def _arg_max(l):
